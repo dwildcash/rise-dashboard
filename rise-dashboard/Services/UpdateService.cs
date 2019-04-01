fb8ff9bb147d8f02fb76601686d51548a2da99a5
dwildcash <dwildcash@prontomail.com>
2019-04-01 12:06:52 -0400

    |Added !Info

--- rise-dashboard/Services/UpdateService.cs
+++ rise-dashboard/Services/UpdateService.cs
@@ -1,7 +1,11 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
+using rise.Models;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 
@@ -12,11 +16,12 @@ namespace Telegram.Bot.Examples.DotNetCoreWebHook.Services
         private readonly IBotService _botService;
         private readonly ILogger<UpdateService> _logger;
 
-        public UpdateService(IBotService botService, ILogger<UpdateService> logger)
+        public UpdateService(IBotService botService, ILogger<UpdateService> logger, UserManager<ApplicationUser> userManager)
         {
             _botService = botService;
             _logger = logger;
         }
+
 
         public async Task EchoAsync(Update update)
         {
@@ -27,28 +32,47 @@ namespace Telegram.Bot.Examples.DotNetCoreWebHook.Services
 
             var message = update.Message;
 
-            _logger.LogInformation("Received Message from {0}", message.Chat.Id);
-
-            if (message.Type == MessageType.Text)
+            string command = string.Empty;
+       
+            // Match Command
+            if (Regex.Matches(message.Text, @"!(\S+)\s?").Count > 0)
             {
-                // Echo each Message
-                await _botService.Client.SendTextMessageAsync(message.Chat.Id, message.Text);
+                command = Regex.Matches(message.Text.ToUpper(), @"!(\S+)\s?")[0].ToString().Trim();
             }
-            else if (message.Type == MessageType.Photo)
+
+            // Info command
+            if (command == "!INFO")
             {
-                // Download Photo
-                var fileId = message.Photo.LastOrDefault()?.FileId;
-                var file = await _botService.Client.GetFileAsync(fileId);
-
-                var filename = file.FileId + "." + file.FilePath.Split('.').Last();
-
-                using (var saveImageStream = System.IO.File.Open(filename, FileMode.Create))
-                {
-                    await _botService.Client.DownloadFileAsync(file.FilePath, saveImageStream);
-                }
-
-                await _botService.Client.SendTextMessageAsync(message.Chat.Id, "Thx for the Pics");
+                await cmd_Info(message.Chat.Id);
             }
         }
+
+
+
+        /// <summary>
+        /// Send Info
+        /// </summary>
+        /// <param name="chatId"></param>
+        /// <returns></returns>
+        private async Task cmd_Info(long chatId)
+        {
+            var strResponse = "<b>RiseVison Information/Tools</b>" + Environment.NewLine +
+                "<b>Rise Website</b> - https://rise.vision" + Environment.NewLine +
+                "<b>Rise Explorer</b> - https://explorer.rise.vision/" + Environment.NewLine +
+                "<b>Rise GitHub</b> - https://github.com/RiseVision" + Environment.NewLine +
+                "<b>Rise Medium</b> - https://medium.com/rise-vision" + Environment.NewLine +
+                "<b>Rise Web Wallet</b> - https://wallet.rise.vision" + Environment.NewLine +
+                "<b>Rise Twitter</b> - https://twitter.com/RiseVisionTeam" + Environment.NewLine +
+                "<b>Rise Force Game</b> - http://duhec.net/riseForce" + Environment.NewLine +
+                "<b>Rise Dashboard</b> - https://rise.coinquote.io" + Environment.NewLine +
+                "<b>Rise Telegram</b> - https://t.me/risevisionofficial" + Environment.NewLine +
+                "<b>Rise Slack</b> - https://risevision.slack.com/" + Environment.NewLine +
+                "<b>Rise Discord</b> - https://discord.gg/6jyWQnJ" + Environment.NewLine +
+                "<b>Rise BitcoinTalk</b> - https://bitcointalk.org/index.php?topic=3211240.200" + Environment.NewLine +
+                "<b>Rise Intro Youtube</b> - https://www.youtube.com/watch?v=wZ2vIGl_gCM&feature=youtu.be" + Environment.NewLine +
+                "<b>Rise Telegram Tipping service</b> -!help";
+                 await _botService.Client.SendTextMessageAsync(chatId, strResponse, ParseMode.Html);
+        }
+
     }
 }
