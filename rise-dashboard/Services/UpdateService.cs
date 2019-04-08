342be51155cc9393c77be47a7ef2aab14acd33d7
dwildcash <dwildcash@protonmail.com>
2019-04-07 20:49:56 -0400

    Restrict

--- rise-dashboard/Services/UpdateService.cs
+++ rise-dashboard/Services/UpdateService.cs
@@ -88,7 +88,7 @@ namespace Rise.Services
 
             try
             {
-                if (appuser.UserName != "dwildcash" && appuser.UserName != "oregonpop" && appuser.UserName != "Showrulz" && appuser.UserName != "Lok35h")
+                if (appuser.UserName != "dwildcash")
                 {
                     return;
                 }
