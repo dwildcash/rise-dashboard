ada3ff3a00e840fcd31fcd7e577a70e56bcbaae7
dwildcash <dwildcash@prontomail.com>
2019-04-30 15:16:32 -0400

    Fix

--- rise-dashboard/gulpfile.js
+++ rise-dashboard/gulpfile.js
@@ -16,10 +16,6 @@ var deps = {
         "/dist/css/bootstrap.min.css": "css",
         "/dist/js/bootstrap.bundle.min.js": "js"
     },
-    "bootstrap-slider": {
-        "/dist/css/bootstrap-slider.min.css": "css",
-        "/dist/bootstrap-slider.min.js": "js"
-    },
     "odometer": {
         "/themes/odometer-theme-slot-machine.css": "css",
         "/odometer.min.js": "js"
