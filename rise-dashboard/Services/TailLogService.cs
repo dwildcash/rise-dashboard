78bfa8b03d339918e935cb4d414fef3d14a55316
dwildcash <dwildcash@prontomail.com>
2019-06-11 13:29:24 -0400

    fix

--- rise-dashboard/Services/TailLogService.cs
+++ rise-dashboard/Services/TailLogService.cs
@@ -4,22 +4,23 @@ using rise.Hubs;
 using rise.Models;
 using System;
 using System.IO;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Text;
 
 namespace rise.Services
 {
     public class TailLogService : BackgroundService
     {
         private readonly IHubContext<NotificationHub> _notificationHub;
-        string filename = "";
-        FileSystemWatcher fileSystemWatcher = null;
-        long previousSeekPosition;
+        private string filename = "";
+        private FileSystemWatcher fileSystemWatcher = null;
+        private long previousSeekPosition;
 
         public delegate void MoreDataHandler(object sender, string newData);
-        public event MoreDataHandler MoreData;
+
         private int maxBytes = 1024 * 16;
+
         public int MaxBytes
         {
             get { return this.maxBytes; }
@@ -31,24 +32,21 @@ namespace rise.Services
             _notificationHub = notificationHub;
         }
 
-
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             try
             {
+                this.filename = AppSettingsProvider.NodeLogFile;
+                FileInfo targetFile = new FileInfo(filename);
 
-                    this.filename = AppSettingsProvider.NodeLogFile;
-                    FileInfo targetFile = new FileInfo(filename);
+                previousSeekPosition = 0;
 
-                    previousSeekPosition = 0;
-
-                    fileSystemWatcher = new FileSystemWatcher();
-                    fileSystemWatcher.IncludeSubdirectories = false;
-                    fileSystemWatcher.Path = targetFile.DirectoryName;
-                    fileSystemWatcher.Filter = targetFile.Name;
-                    fileSystemWatcher.Changed += new FileSystemEventHandler(TargetFile_Changed);
-                    fileSystemWatcher.EnableRaisingEvents = true;
-
+                fileSystemWatcher = new FileSystemWatcher();
+                fileSystemWatcher.IncludeSubdirectories = false;
+                fileSystemWatcher.Path = targetFile.DirectoryName;
+                fileSystemWatcher.Filter = targetFile.Name;
+                fileSystemWatcher.Changed += new FileSystemEventHandler(TargetFile_Changed);
+                fileSystemWatcher.EnableRaisingEvents = true;
             }
             catch (Exception ex)
             {
@@ -58,30 +56,28 @@ namespace rise.Services
 
         public void TargetFile_Changed(object source, FileSystemEventArgs e)
         {
+            //read from current seek position to end of file
+            byte[] bytesRead = new byte[maxBytes];
+            FileStream fs = new FileStream(this.filename, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            if (fs.Length > maxBytes)
+            {
+                this.previousSeekPosition = fs.Length - maxBytes;
+            }
+            this.previousSeekPosition = (int)fs.Seek(this.previousSeekPosition, SeekOrigin.Begin);
+            int numBytes = fs.Read(bytesRead, 0, maxBytes);
+            fs.Close();
+            this.previousSeekPosition += numBytes;
 
-                //read from current seek position to end of file
-                byte[] bytesRead = new byte[maxBytes];
-                FileStream fs = new FileStream(this.filename, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-                if (fs.Length > maxBytes)
-                {
-                    this.previousSeekPosition = fs.Length - maxBytes;
-                }
-                this.previousSeekPosition = (int)fs.Seek(this.previousSeekPosition, SeekOrigin.Begin);
-                int numBytes = fs.Read(bytesRead, 0, maxBytes);
-                fs.Close();
-                this.previousSeekPosition += numBytes;
+            StringBuilder sb = new StringBuilder();
 
-                StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < numBytes; i++)
+            {
+                sb.Append((char)bytesRead[i]);
+            }
 
-                for (int i = 0; i < numBytes; i++)
-                {
-                    sb.Append((char)bytesRead[i]);
-                }
-
-                //call delegates with the string
-                //this.MoreData(this, sb.ToString());
-                _notificationHub.Clients.All.SendAsync("Send", sb.ToString());
-
+            //call delegates with the string
+            //this.MoreData(this, sb.ToString());
+            _notificationHub.Clients.All.SendAsync("Send", sb.ToString());
         }
     }
 }
