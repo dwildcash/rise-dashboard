6b68b48b3bd10df711a83659f04d820384b7aed1
dwildcash <dwildcash@prontomail.com>
2019-06-11 08:40:37 -0400

    fix

--- rise-dashboard/Models/TailLog.cs
+++ rise-dashboard/Models/TailLog.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace rise.Models
 {
     public static class TailLog
     {
-        public static DateTime LogDate  {get; set; }
+        public static DateTime LogDate { get; set; }
         public static string LogLine { get; set; }
     }
 }
