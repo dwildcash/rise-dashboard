9a9899534dc4c52fdcb0e9a69d358db40edac155
dwildcash <dwildcash@prontomail.com>
2019-06-11 16:09:58 -0400

    fix

--- rise-dashboard/Services/TailLogService.cs
+++ rise-dashboard/Services/TailLogService.cs
@@ -4,6 +4,7 @@ using rise.Hubs;
 using rise.Models;
 using System;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -38,10 +39,7 @@ namespace rise.Services
             {
                 this.filename = AppSettingsProvider.NodeLogFile;
                 FileInfo targetFile = new FileInfo(filename);
-                byte[] bytesRead = new byte[maxBytes];
-                FileStream fs = new FileStream(this.filename, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-                this.previousSeekPosition = fs.Length;
-                fs.Close();
+               
 
 
                 fileSystemWatcher = new FileSystemWatcher();
@@ -59,28 +57,13 @@ namespace rise.Services
 
         public void TargetFile_Changed(object source, FileSystemEventArgs e)
         {
-            //read from current seek position to end of file
-            byte[] bytesRead = new byte[maxBytes];
             FileStream fs = new FileStream(this.filename, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-            if (fs.Length > maxBytes)
-            {
-                this.previousSeekPosition = fs.Length - maxBytes;
-            }
-            this.previousSeekPosition = (int)fs.Seek(this.previousSeekPosition, SeekOrigin.Begin);
-            int numBytes = fs.Read(bytesRead, 0, maxBytes);
-            fs.Close();
-            this.previousSeekPosition += numBytes;
 
-            StringBuilder sb = new StringBuilder();
-
-            for (int i = 0; i < numBytes; i++)
-            {
-                sb.Append((char)bytesRead[i]);
-            }
+            var lastLine = File.ReadLines(AppSettingsProvider.NodeLogFile).Last();
 
             //call delegates with the string
             //this.MoreData(this, sb.ToString());
-            _notificationHub.Clients.All.SendAsync("Send", sb.ToString());
+            _notificationHub.Clients.All.SendAsync("Send", lastLine);
         }
     }
 }
