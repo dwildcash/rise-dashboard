c63eb639deb2c1699dd08456e99a864135ba32f6
dwildcash <dwildcash@prontomail.com>
2019-06-11 13:12:24 -0400

    fix

--- rise-dashboard/Services/TailLogService.cs
+++ rise-dashboard/Services/TailLogService.cs
@@ -6,60 +6,82 @@ using System;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Text;
 
 namespace rise.Services
 {
     public class TailLogService : BackgroundService
     {
         private readonly IHubContext<NotificationHub> _notificationHub;
+        string filename = "";
+        FileSystemWatcher fileSystemWatcher = null;
+        long previousSeekPosition;
+
+        public delegate void MoreDataHandler(object sender, string newData);
+        public event MoreDataHandler MoreData;
+        private int maxBytes = 1024 * 16;
+        public int MaxBytes
+        {
+            get { return this.maxBytes; }
+            set { this.maxBytes = value; }
+        }
 
         public TailLogService(IHubContext<NotificationHub> notificationHub)
         {
             _notificationHub = notificationHub;
         }
 
+
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             try
             {
-                while (!stoppingToken.IsCancellationRequested)
-                {
-                    using (StreamReader reader = new StreamReader(new FileStream(AppSettingsProvider.NodeLogFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)))
-                    {
-                        //start at the end of the file
-                        long lastMaxOffset = reader.BaseStream.Length;
 
-                        while (true)
-                        {
-                            Thread.Sleep(100);
+                    this.filename = AppSettingsProvider.NodeLogFile;
+                    FileInfo targetFile = new FileInfo(filename);
 
-                            //if the file size has not changed, idle
-                            if (reader.BaseStream.Length == lastMaxOffset)
-                            {
-                                continue;
-                            }
+                    previousSeekPosition = 0;
 
-                            //seek to the last max offset
-                            reader.BaseStream.Seek(lastMaxOffset, SeekOrigin.Begin);
+                    fileSystemWatcher = new FileSystemWatcher();
+                    fileSystemWatcher.IncludeSubdirectories = false;
+                    fileSystemWatcher.Path = targetFile.DirectoryName;
+                    fileSystemWatcher.Filter = targetFile.Name;
+                    fileSystemWatcher.Changed += new FileSystemEventHandler(TargetFile_Changed);
+                    fileSystemWatcher.EnableRaisingEvents = true;
 
-                            //read out of the file until the EOF
-                            string line;
-
-                            while ((line = reader.ReadLine()) != null)
-                            {
-                                await _notificationHub.Clients.All.SendAsync("Send", line);
-                            }
-
-                            //update the last max offset
-                            lastMaxOffset = reader.BaseStream.Position;
-                        }
-                    }
-                }
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Received Exception from TailLogService {0}", ex.Message);
             }
+        }
+
+        public void TargetFile_Changed(object source, FileSystemEventArgs e)
+        {
+
+                //read from current seek position to end of file
+                byte[] bytesRead = new byte[maxBytes];
+                FileStream fs = new FileStream(this.filename, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                if (fs.Length > maxBytes)
+                {
+                    this.previousSeekPosition = fs.Length - maxBytes;
+                }
+                this.previousSeekPosition = (int)fs.Seek(this.previousSeekPosition, SeekOrigin.Begin);
+                int numBytes = fs.Read(bytesRead, 0, maxBytes);
+                fs.Close();
+                this.previousSeekPosition += numBytes;
+
+                StringBuilder sb = new StringBuilder();
+
+                for (int i = 0; i < numBytes; i++)
+                {
+                    sb.Append((char)bytesRead[i]);
+                }
+
+                //call delegates with the string
+                //this.MoreData(this, sb.ToString());
+                _notificationHub.Clients.All.SendAsync("Send", sb.ToString());
+
         }
     }
 }
