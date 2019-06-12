71f649e24f98468cac9fd567be7c35af4f57d9d0
dwildcash <dwildcash@prontomail.com>
2019-06-12 12:57:54 -0400

    fix

--- rise-dashboard/wwwroot/js/NotificationHub.js
+++ rise-dashboard/wwwroot/js/NotificationHub.js
@@ -4,19 +4,14 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/Hub/notificationHu
 
 connection.on("Send", function (message) {
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    var encodedMsg = msg;
-    var p = document.createElement("p");
-    p.textContent = encodedMsg;
-    document.getElementById("messagesList").appendChild(p);
-
-    var element = document.getElementById("messagesList");
+    var element = document.getElementById("master");
+    element.innerHTML += msg + '<br>';
     element.scrollTop = element.scrollHeight - element.clientHeight;
 });
 
 connection.start().then(function () {
-    var li = document.createElement("li");
-    li.textContent = "Connected to server...";
-    document.getElementById("messagesList").appendChild(li);
+    var element = document.getElementById("master");
+    element.innerHTML += "<p class='success'>Connected to server</p>";
 }).catch(function (err) {
     return console.error(err.toString());
 });
