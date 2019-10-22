53abc401e9693476f76d7ef70140dc3b5974ee3f
dwildcash <dwildcash@prontomail.com>
2019-10-22 12:32:07 -0400

    fix api

--- rise-dashboard/Code/DataFetcher/DelegateFetcher.cs
+++ rise-dashboard/Code/DataFetcher/DelegateFetcher.cs
@@ -23,44 +23,8 @@
                 // Retreive Quote
                 using (var hc = new HttpClient())
                 {
-                    var result = JObject.Parse(await hc.GetStringAsync(AppSettingsProvider.APIUrl + "/api/delegates?orderBy=rank:asc"));
+                    var result = JObject.Parse(await hc.GetStringAsync(AppSettingsProvider.APIUrl + "/api/delegates"));
                     var delegateResult = JsonConvert.DeserializeObject<DelegateResult>(result.ToString());
-
-                    var result200to399 = JObject.Parse(await hc.GetStringAsync(AppSettingsProvider.APIUrl + "/api/delegates?orderBy=rank:asc&offset=199"));
-                    var delegate200to399 = JsonConvert.DeserializeObject<DelegateResult>(result200to399.ToString());
-
-                    var result400to599 = JObject.Parse(await hc.GetStringAsync(AppSettingsProvider.APIUrl + "/api/delegates?orderBy=rank:asc&offset=398"));
-                    var delegate400to599 = JsonConvert.DeserializeObject<DelegateResult>(result400to599.ToString());
-
-                    var result600to799 = JObject.Parse(await hc.GetStringAsync(AppSettingsProvider.APIUrl + "/api/delegates?orderBy=rank:asc&offset=597"));
-                    var delegate600to799 = JsonConvert.DeserializeObject<DelegateResult>(result600to799.ToString());
-
-                    var result800to999 = JObject.Parse(await hc.GetStringAsync(AppSettingsProvider.APIUrl + "/api/delegates?orderBy=rank:asc&offset=796"));
-                    var delegate800to999 = JsonConvert.DeserializeObject<DelegateResult>(result800to999.ToString());
-
-                    // Merge Delegates 200 to 399
-                    foreach (var o in delegate200to399.Delegates)
-                    {
-                        delegateResult.Delegates.Add(o);
-                    }
-
-                    // Merge Delegates 400 to 599
-                    foreach (var o in delegate400to599.Delegates)
-                    {
-                        delegateResult.Delegates.Add(o);
-                    }
-
-                    // Merge Delegates 600 to 799
-                    foreach (var o in delegate600to799.Delegates)
-                    {
-                        delegateResult.Delegates.Add(o);
-                    }
-
-                    // Merge Delegates 800 to 999
-                    foreach (var o in delegate800to999.Delegates)
-                    {
-                        delegateResult.Delegates.Add(o);
-                    }
 
                     return delegateResult.Success ? delegateResult : null;
                 }
