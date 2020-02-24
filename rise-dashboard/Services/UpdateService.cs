68c7e308212bb93ec1cc1eb89f85702879a6a94f
dwildcash <dwildcash@prontomail.com>
2020-02-24 15:29:10 -0500

    fix

--- rise-dashboard/Services/UpdateService.cs
+++ rise-dashboard/Services/UpdateService.cs
@@ -608,7 +608,7 @@ namespace Rise.Services
 
                     if (walletAccountResult != null)
                     {
-                        double d = @Math.Round((double)mydelegate.ForgingChance / 100.0 * (double)AppSettingsProvider.CoinRewardDay * double.Parse(walletAccountResult.account.Balance) / ((mydelegate.VotesWeight / 100000000))) * (double)pool.Share / 100, 1);
+                        double d = @Math.Round((double)mydelegate.ForgingChance / 100.0 * (double)AppSettingsProvider.CoinRewardDay * double.Parse(walletAccountResult.account.Balance) / (mydelegate.VotesWeight / 100000000) * (double)pool.Share / 100, 3);
                         estimateAward = "est. daily reward:" + d + " Rise";
                     }
                    
