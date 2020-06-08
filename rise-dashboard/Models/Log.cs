4c2717de1dfebd69aee27ad9996ca655b0c5ef47
Emmanuel Pare <emmanuel.pare@gmail.com>
2020-06-08 11:04:54 -0400

    added new Log

---
+++ rise-dashboard/Models/Log.cs
@@ -0,0 +1,25 @@
+﻿namespace rise.Models
+{
+    using Newtonsoft.Json;
+    using System;
+    using System.Collections.Generic;
+
+    public partial class Log
+    {
+        private int Id { get; set; }
+
+        private DateTime Date { get; set; }
+
+        private string Message { get; set; }
+    }
+
+    // Log a Message
+    public partial class Log
+    {
+        public void LogMessage(string Message)
+        {
+            this.Date = DateTime.Now;
+            this.Message = Message;
+        }
+    }
+}
