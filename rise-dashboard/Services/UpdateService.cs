2f017d15bd80c739428c58a6a8b72f92fd9a90df
Emmanuel Pare <emmanuel.pare@gmail.com>
2020-12-29 13:47:35 -0500

    Fix display exchanges

--- rise-dashboard/Services/UpdateService.cs
+++ rise-dashboard/Services/UpdateService.cs
@@ -899,7 +899,9 @@ namespace rise.Services
 
                 await _botService.Client.SendChatActionAsync(message.Chat.Id, ChatAction.Typing);
                 var strResponse = "<b>-= Current Rise Exchanges =-</b>" + Environment.NewLine +
-                                  "<b>Xt.com</b> - https://www.xt.com (24h V:" + volXtcom.ToString("N0") + " - Price:" + priceXtcom + " sat)" + Environment.NewLine +
+                                  "<b>Xt.com</b> - https://www.xt.com" + Environment.NewLine +
+                                  "Price (sat): <b>" + priceXtcom + "</b>" + Environment.NewLine +
+                                  "Vol (24H): <b>" +  volXtcom.ToString("N0") + "/<b>" + Environment.NewLine +
                                   "Bitcoin Price: <b>" + Math.Round(double.Parse(CoinbaseBtcQuote.Current.amount), 2) + "$</b> (Coinbase)";
 
                 await _botService.Client.SendTextMessageAsync(message.Chat.Id, strResponse, ParseMode.Html);
