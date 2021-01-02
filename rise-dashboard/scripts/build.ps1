1b70b63ea7ffb174744a39dfe510355c8ce5a381
Emmanuel Pare <emmanuel.pare@gmail.com>
2021-01-02 10:15:12 -0500

    fix build script

--- rise-dashboard/scripts/build.ps1
+++ rise-dashboard/scripts/build.ps1
@@ -1,4 +1,4 @@
-#!/bin/pwsh
+#!/usr/bin/pwsh
 
 write-host -ForegroundColor Magenta "Stopping Dashboard..."
 systemctl stop kestrel-rise-dashboard.service
@@ -6,12 +6,14 @@ systemctl stop kestrel-rise-dashboard.service
 cd /var/www/rise-dashboard/
 pkill dotnet
 
-write-host -ForegroundColor Green "Fetching Latest version from github.."
+write-host -ForegroundColor Green "Fetching Latest version from github..."
 git pull
 
 write-host -ForegroundColor Green "Building new version..."
 dotnet build
 chown -R www-data:www-data /var/www/rise-dashboard
 
-write-host -ForegroundColor Magenta "Restarting Dashboard"
+write-host -ForegroundColor Magenta "Restarting Dashboard..."
 systemctl start kestrel-rise-dashboard.service
+
+write-host -ForegroundColor Magenta "Completed."
